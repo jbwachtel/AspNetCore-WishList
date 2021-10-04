59899551d79124cae648506390b6aa90b81afdc6
jbwachtel <89084916+jbwachtel@users.noreply.github.com>
2021-10-04 12:06:11 -0400

    Adding in models and data classes

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDBContext : DBContext
+    {
+        public ApplicationDBContext()
+        {
+        }
+
+        public ApplicationDBContext() : base(options)
+        {
+            var DbContextOptions(options);
+        }
+
+        public var Items(DbSet<Item>)
+        {
+            get{}
+            set{ }
+        }
+    }
+}
---
+++ WishList/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+        
+
+        public string Description(required, MaxLength(50))
+        {
+
+        }
+        
+    }
+
+    
+}
